010f74a5b6605778d247854829daa572f7310560
muwongelawrence-tech <muwongelawrence44@gmail.com>
2022-03-13 06:46:25 +0300

    addition to the pandas module

--- classes and modules/Pandas2.py
+++ classes and modules/Pandas2.py
@@ -24,3 +24,40 @@ print("Value for apples: ", fruits['apples'], "\n")
 
 # Series also supports array-style operations such as slicing:
 print(fruits['bananas':'strawberries'])
+
+print("------------------working with data frames------------------")
+
+data_s1 = pd.Series([12, 24, 33, 15],
+           index=['apples', 'bananas', 'strawberries', 'oranges'])
+
+# 'quantity' is the name for our column
+dataframe1 = pd.DataFrame(data_s1, columns=['quantity'])
+print(dataframe1)
+
+print("------------Constructing a DataFrame From a Dictionary#-------------")
+dict = {"country": ["Norway", "Sweden", "Spain", "France"],
+       "capital": ["Oslo", "Stockholm", "Madrid", "Paris"],
+       "SomeColumn": ["100", "200", "300", "400"]}
+
+data = pd.DataFrame(dict)
+print(data)
+
+print("------------Constructing a data frame from a dictionary of series-------------")
+quantity = pd.Series([12, 24, 33, 15],
+                     index=['apples', 'bananas', 'strawberries', 'oranges'])
+
+price = pd.Series([4, 4.5, 8, 7.5],
+                  index=['apples', 'bananas', 'strawberries', 'oranges'])
+
+df = pd.DataFrame({'quantity': quantity,
+                   'price': price})
+print(df)
+
+print(" constructing a dataframe from a file read from the directory.")
+# Given we have a file called data1.csv in our working directory:
+df = pd.read_csv('data1.csv')
+
+#given json data
+df = pd.read_json('data2.json')
+
+print("---------Pandas DataFrame Operations - Read, View and Extract Information.----------")
