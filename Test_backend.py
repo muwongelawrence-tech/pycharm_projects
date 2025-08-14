9567ea73ae120f802595641ad136aae79f327e87
muwongelawrence-tech <muwongelawrence44@gmail.com>
2025-08-14 07:53:38 +0300

    changing the order a little bit

--- Test_backend.py
+++ Test_backend.py
@@ -1,28 +1,3 @@
-# import requests
-# import time
-
-# # URL to be pinged
-# url = "https://prechohub-server.onrender.com"
-
-# def ping_url():
-#     try:
-#         response = requests.get(url)
-#         if response.status_code == 200:
-#             print(f"[{time.ctime()}] Successfully pinged {url}. Status: {response.status_code}")
-#         else:
-#             print(f"[{time.ctime()}] Ping failed. Status: {response.status_code}")
-#     except requests.exceptions.RequestException as e:
-#         print(f"[{time.ctime()}] Error pinging {url}: {e}")
-
-# if __name__ == "__main__":
-#     print("Starting URL pinger...")
-#     while True:
-#         ping_url()
-#         time.sleep(600)  # Wait for 10 minutes (600 seconds) before the next ping
-# 
-
-# run script in your terminal using ::--> python3 Test_backend.py 
-
 import requests
 import time
 
@@ -47,6 +22,31 @@ if __name__ == "__main__":
         for url in urls:
             ping_url(url)
         time.sleep(600)  # Wait for 10 minutes (600 seconds) before the next round
+
+# import requests
+# import time
+
+# # URL to be pinged
+# url = "https://prechohub-server.onrender.com"
+
+# def ping_url():
+#     try:
+#         response = requests.get(url)
+#         if response.status_code == 200:
+#             print(f"[{time.ctime()}] Successfully pinged {url}. Status: {response.status_code}")
+#         else:
+#             print(f"[{time.ctime()}] Ping failed. Status: {response.status_code}")
+#     except requests.exceptions.RequestException as e:
+#         print(f"[{time.ctime()}] Error pinging {url}: {e}")
+
+# if __name__ == "__main__":
+#     print("Starting URL pinger...")
+#     while True:
+#         ping_url()
+#         time.sleep(600)  # Wait for 10 minutes (600 seconds) before the next ping
+# 
+
+# run script in your terminal using ::--> python3 Test_backend.py 
 
 
 
